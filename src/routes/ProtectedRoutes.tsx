6fdf3bc02a40166843b95379801d944f08ecc2cf
Nkematu Bonaventure <74823094+ChuloCr8v@users.noreply.github.com>
2025-08-18 12:17:31 +0100

    `Removed unnecessary line in ProtectedRoutes component`

--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -7,7 +7,6 @@ import Departments from "../views/departments/Departments";
 import Testing from "../views/employees/Testing";
 
 const ProtectedRoutes = () => {
-  a;
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
